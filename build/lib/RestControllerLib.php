93557d180e6370427d4f50c44709126b6c6756cd
playerthree <andy@playerthree.com>
2013-05-29 11:18:43 +0100

    updated RestRequest

---
+++ build/lib/RestControllerLib.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Andy
+ * Date: 29/05/13
+ * Time: 10:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+class RestControllerLib
+{
+    public static function error($a, $b)
+    {
+        return "error";
+    }
+}
