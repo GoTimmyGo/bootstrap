d5e4b941852fcb9ec7d407269b5deab0e4e828c7
GoTimmyGo <andy@andyscript.com>
2013-06-01 00:44:48 +0100

    incorporated Base Admin 2.0 theme

--- build/index.php
+++ build/index.php
@@ -3,26 +3,70 @@
     <head>
         <meta charset="utf-8">
         <title>Andyscript</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <meta name="apple-mobile-web-app-capable" content="yes">
         <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
         <link rel="stylesheet" href="css/bootstrap-responsive.min.css" type="text/css">
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/jquery-ui-1.10.0.custom.min.css">
+        <link rel="stylesheet" href="css/base-admin-2.css">
+        <link rel="stylesheet" href="css/base-admin-2-responsive.css">
     </head>
     <body>
-        <div class="container">
-            <div class="navbar">
-                <div class="navbar-inner">
-                    <a class="brand" href="index.php">Andyscript.</a>
-                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container">
+                    <a class="brand" href="index.php">AndyMin <sup>0.1</sup></a>
+                    <a type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <i class="icon-cog"></i>
+                    </a>
                     <div class="nav-collapse collapse">
-                        <ul class="nav">
-                            <li class="active"><a href="#">Home</a></li>
-                            <li><a href="#">Portfolio</a></li>
-                            <li><a href="#">Labs</a></li>
-                            <li><a href="#">Contact</a></li>
+                        <ul class="nav pull-right">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="icon-cog"></i>
+                                    Settings
+                                    <b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Account Settings</a></li>
+                                    <li><a href="#">Privacy Settings</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Help</a></li>
+                                </ul>
+                            </li>
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="icon-user"></i>
+                                    Andy Anderson
+                                    <b class="caret"></b>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">My Profile</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">Logout</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="subnavbar">
+            <div class="subnavbar-inner">
+                <div class="container">
+                    <a class="btn-subnavbar collapsed" data-toggle="collapse" data-target=".subnav-collapse">
+                        <i class="icon-reorder"></i>
+                    </a>
+                    <div class="subnav-collapse collapse">
+                        <ul class="mainnav">
+                            <li class="active">
+                                <a href="index.php">
+                                    <i class="icon-home"></i>
+                                    <span>Home</span>
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
